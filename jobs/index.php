cdb2b6ea04981f82018e3bc0d2d572faa86335c5
jayvachon <jaylvachon@gmail.com>
2015-01-05 15:12:10 -0500

    update jobs

--- jobs/index.php
+++ jobs/index.php
@@ -12,36 +12,18 @@
 						<h1>Come Work with Us!</h1>
 						<h3>We are looking to fill the following positions as soon as possible:</h3>
                         
-						<h4><span>Position:</span> Javascript Developer</h4>
-						<h4><span>Type:</span> Full Time </h4>
+						<h4><span>Position:</span> Lab Coordinator</h4>
+						<h4><span>Type:</span> Full Time</h4>
 						<p>
-							<span>Description:</span> We are looking for a dynamic full time javascript programmer who can assist in lab projects ranging from online games to data visualizations to civic APIs. This person will work with the lab and its partners at the UN, the Red Cross, the City of Boston, and others to create experimental and playful solutions to local and global civic issues. This position will involve working closely with our visual designers, content experts, and game designers, using open web technologies. 
+							<span>Description:</span> The Lab Coordinator is responsible for maintaining productive and vibrant relationships and workflows within the Engagement Lab community. This position is composed of three primary duties: administrative support for lab operations and faculty; coordination of Engagement Lab events, groups, and space; and social media outreach. In addition to these specific duties, the Lab Coordinator's overall objective is to maintain a welcoming, well-organized, and vibrant lab community of staff, students, faculty, and partners. This position will report directly to the managing director and is asked to attend and help organize weekly meetings on organizational and project-specific updates.  
 						</p>
 						<p>
-							Skills and experience necessary for this role:
-							<ul>
-								<li>
-									Fluent in JavaScript	
-								</li>
-								<li>
-									Familiarity with jQuery
-								</li>
-								<li>
-									HTML and CSS skills
-								</li>
-								<li>
-									Experience or the ability to pick up NodeJS quickly
-								</li>
-								<li>
-									Familiarity with simple database concepts (MongoDB skills a plus)
-								</li>
-							</ul>
+							We are looking for a highly-motivated self-starter who will not only keep the lab functioning on a day-to-day basis, but help strategically rethink and redesign systems and protocols for a better lab. The ideal candidate will have experience in budget management, HR, event coordination, media production, and marketing. Moreover, we seek a person with a passion for civic engagement and media who will inject creativity, energy, and a commitment to teamwork into the lab and its projects. 
 						</p>
 						<p>
 							Please send an email to <span>steve [at] engagementgamelab.org</span> with a link to your portfolio and a brief description about your background and experience.
 						</p>
                         <br />
-                        
 					</div>
 				</div>
 			</div>
