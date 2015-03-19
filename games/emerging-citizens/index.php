274bc7d4d3cf93713b8b7f1cf5ee8eab6e6b5cac
absolutelyalex <alex.eby5@gmail.com>
2015-03-19 15:45:27 -0400

    added Emerging Citizens page to the game section of the website

---
+++ games/emerging-citizens/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<?php include '../../head.php' ?>
+	</head>
+	<body>
+		<div id='wrap'>
+			<?php include '../../nav.php' ?>
+			<div class='whatIsIt'>
+				<div class='container'>
+					<div class='row'>
+						<div class='span4'>
+							<div class='projectLogo'>
+								<img src='../../img/projects/logos/rh.png'>
+							</div>
+						</div>
+						<div class='span8 projectDescription'>
+							<p>
+								The <strong>Emerging Citizens</strong> project is a collaborative team-based engagement game and curriculum. Students develop media literacy skills to engage with a changing media landscape that blurs the lines between public and private, entertainment and advocacy, and information and advertising.
+							</p>
+						</div>
+					</div>
+					<div class='row'>
+						<div class='span6 projectContext'>
+							<h2 class='funTitle'>
+								Context
+							</h2>
+							<p>
+								Students today are increasingly using social media to create and share information, pursue social relationships, engage in leisure activities, and make important decisions about personal privacy. As consumers, however, they often lack awareness of how information’s purpose, characteristics and quality differ across delivery platforms and providers. As producers, they are often unfamiliar with the ways that new media can facilitate broad participation in public life.
+							</p>
+							<p>
+								This curriculum and game are designed for educators to help students build the competencies to navigate this landscape: critically evaluating new media; participating creatively and responsibly; and educating, organizing, and mobilizing to address pressing community needs. This work is part of the Boston pilot of Ask for Evidence, a public information project that helps people evaluate the evidence behind news stories, marketing claims, and public policies.
+							</p>
+						</div>
+							<div class='span6 videoContainer'>
+								<img class='inlineImageRightBig' src='../../img/projects/emerging-citizens/mountainpeople.png'>
+							</div>
+					</div>
+				</div>
+			</div>
+			<div class='container projectPage'>
+				<div class='theGame'>
+					<div class='row'>
+						<div class='span12'>
+							<h2 class='funTitle'>The Project</h2>
+							<p>
+								<span>The Curricular Arc:</span>
+								<img class='inlineImageRightBig' src='../../img/projects/emerging-citizens/curriculummap.png'>
+								Students choose an issue or problem with global and local resonance. Then, they move through a series of four modules, each beginning with a lesson and culminating in a round of game play. Lessons onboard players with the hands-on skills and content knowledge they will need to complete game round challenges. The modules are cumulative, taking students on a journey from exploration of personal stories to critical analysis of relevant media to pitching actionable solutions or approaches to the issue at hand. In culmination, students implement one of these solutions in their community.
+								<br><br><br><br>
+							</p>
+							<p>
+								<span>Playing the Game:</span>
+								<img class='inlineImageLeftBig' src='../../img/projects/emerging-citizens/gameplayflow.png'>
+								In teams, students progress through four rounds of play, each addressing a distinct aspect of media literacy. Teams create challenges for each other to complete and then select their favorites. Teams complete their chosen challenges and evaluate each other’s responses.
+							</p>
+							<p>
+								Teams play a short mini-game to determine which one gets to choose their challenge first. Then, for consistency in the evaluation stage, the other team completes the same challenges. After choosing their challenge, teams encounter another element of chance by drawing a random modality card (storyboarding, persuasive letter, media remix, etc.) that guides their response. When teams evaluate each other’s responses, they use rubrics tailored to the goals of that particular module.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class='realActions extraPadding'>
+					<div class='row'>
+						<div class='span12'>
+							<h2 class='funTitle'>The Real-World Action/Results</h2>
+							<p>
+								Using games for classroom learning is not a new practice, but this game and curriculum expand upon that concept by using game play to scaffold real-world action in the form of student media products, pitches, and local campaigns. Students initially develop a critical analysis of needs and opportunities in their own communities and then build the skills to create media about pressing social issues in multiple modalities for targeted audiences.								
+							</p>
+							<p>
+								The Emerging Citizens project weaves together a range of Common Core English Language Arts and Mathematics standards, Next Generation Science Standards’ Science and Engineering Practices, and standards from the National Council for Social Studies’ Framework for College, Career, and Civic Life. Students engaged in this work analyze writing and images, communicate and collaborate frequently, and critically evaluate established discourses and approaches. Throughout the curriculum, students identify and define problems, plan and carry out investigations, interpret data to identify patterns in their environments, and engage in deliberation, reasoned arguments, and media creation to drive local action.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id='push'></div>
+		<?php include '../../footer.php' ?>
+
+    	<!- Los javascripts ->
+    	<script src='/js/libs/lab.js'></script>
+    	<script>
+    		$LAB
+    		.script('/js/libs/jquery.min.js').wait()
+			.script('/js/libs/bootstrap.min.js').wait()
+			.script('/js/libs/fitvids.js').wait()
+			.script('/js/other/vimeo.js');
+    	</script>
+  </body>
+</html>
