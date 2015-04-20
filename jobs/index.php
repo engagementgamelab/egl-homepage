17f6264477f4b6a08e8d0286a2e5c6a20bd0a872
Jay Vachon <jaylvachon@gmail.com>
2015-04-20 13:58:58 -0400

    update jobs

--- jobs/index.php
+++ jobs/index.php
@@ -11,39 +11,60 @@
 					<div class='span12'>
 						<h1>Come Work with Us!</h1>
 						<h3>We are looking to fill the following positions as soon as possible:</h3>
-                        
-						<h4><span>Position:</span> Lab Coordinator</h4>
-						<h4><span>Type:</span> Full Time</h4>
-						<p>
-							<span>Description:</span> The Lab Coordinator is responsible for maintaining productive and vibrant relationships and workflows within the Engagement Lab community. This position is composed of three primary duties: administrative support for lab operations and faculty; coordination of Engagement Lab events, groups, and space; and social media outreach. In addition to these specific duties, the Lab Coordinator's overall objective is to maintain a welcoming, well-organized, and vibrant lab community of staff, students, faculty, and partners. This position will report directly to the managing director and is asked to attend and help organize weekly meetings on organizational and project-specific updates.  
-						</p>
-						<p>
-							We are looking for a highly-motivated self-starter who will not only keep the lab functioning on a day-to-day basis, but help strategically rethink and redesign systems and protocols for a better lab. The ideal candidate will have experience in budget management, HR, event coordination, media production, and marketing. Moreover, we seek a person with a passion for civic engagement and media who will inject creativity, energy, and a commitment to teamwork into the lab and its projects. 
-						</p>
-						<p>
-							Please send an email to <span>steve [at] engagementgamelab.org</span> with a link to your resume and a brief description about your background and experience or apply directly to the Emerson College job portal <a href="https://emerson.peopleadmin.com/postings/8907">here</a>.
-						</p>
-                        <br />
-                        <h4><span>Position:</span> Chief Technology Officer and Lead Coder</h4>
+                        <h4><span>Position:</span> Managing Director</h4>
                         <p>
-                        	<span>Description:</span> The Engagement Lab is a research and design lab at Emerson College focused on inventing playful and critical approaches to civic life both locally and globally. We are looking for a creative, scrupulous, and civic-oriented coder for the new Lab position of Chief Technology Officer and Lead Coder. As the Lab grows and expands its role in the areas of civic technology, <a href="http://engagementlab.emerson.edu/learning/#guidebooks">engagement games</a>, and human computation / data visualization, we seek a leader who can guide the direction of our development projects and technical infrastructure. This position includes working with project teams to manage software development across projects, acting as lead programmer for select projects, and working with lab directors in establishing technical protocols and approaches. Current software development projects include a game about Results-based financing in healthcare for the World Bank, a civic incentive system for the City of Boston, and a mobile deliberation game for Participatory Budgeting. The CTO/LC will also be charged with growing and managing a small team of coders and developers, as well as traveling to conferences and other development community meet-ups.
+                        	<span>Description:</span> The Engagement Lab is searching for a new Managing Director! We are looking for a passionate and creative organizational leader--someone with experience working in fast-paced, applied research environments in both academic and non-academic settings. The ideal candidate has outstanding written and oral communication skills, an eye for good user design, a passion for civic engagement, and an ability to manage multiple projects that meld academic research, design research, and community implementation.
                         </p>
 						<p>
-							The Lab has a stake in experimental methods/frameworks and in contributing to the knowledge base of open source development. Experience with HMTL5 and advanced javascript is a must, and knowledge of existing and emerging back-end frameworks is desirable.
+							The Engagement Lab is an applied research lab at Emerson College that designs and studies media and technology for civic engagement. Our work spans content areas (from urban planning to youth media literacy) and continents (from Boston to Bhutan). Our partners range from the United Nations Development Programme to the City of Boston, and our civic media projects have reached over 30,000 people in over 150 countries. We are committed to an action research approach (see one of our methodology manuals <a href="http://www.google.com/url?q=http%3A%2F%2Fengagementlab.emerson.edu%2Fpdfs%2Fdarg.pdf&sa=D&sntz=1&usg=AFQjCNFzsiqrfFY_eaHFj7nILLn8zOJ4aA">here</a>) and in particular have explored the use of games and playful interventions to engage unconventional stakeholders in meaningful social change (see our manual on “Engagement Games” <a href="http://www.google.com/url?q=http%3A%2F%2Fengagementlab.emerson.edu%2Fpdfs%2Fengagement-game-guide.pdf&sa=D&sntz=1&usg=AFQjCNFqV8LrfgfVM-Vz-7ulgnrt67c_TQ">here</a>). We also run the <a href="http://www.google.com/url?q=http%3A%2F%2Fwww.salzburg.umd.edu%2F&sa=D&sntz=1&usg=AFQjCNGLGfyH4cVIU_d99B1kSuCPkChKaA">Salzburg Academy on Media and Global Change</a>, which brings together top students and professionals from around the world for three weeks every summer to research and create media for global change.
 						</p>
 						<p>
-							While at least four years of professional experience in coding and software management is desired, we remain open to candidates displaying boldness, creativity, and dexterity. The ideal candidate should have:
+							The Engagement Lab Managing Director is responsible for holistically overseeing all Lab projects, spearheading and managing Lab growth, and bridging the gaps between various Lab teams. This entails working closely with Lab PIs in strategically pursuing new foundation and client funding, working with Emerson College administration in pre- and post-award management, and working with the Lab’s team of creatives, project managers, researchers, and students to guide creative and scholarly output. Responsibilities include:
 						</p>
+						<h4>Organizational Oversight</h4>
 						<ul>
-							<li>Advanced knowledge of HTML5 protocols, frameworks, and libraries</li>
-							<li>Knowledge of emerging technologies (ie Node.js ☺) and legacy technologies (ie Flash ☹) a plus</li>
-							<li>Ability to spot, experiment with, evaluate, and implement budding technologies</li>
-							<li>Ability to manage a team of developers and to work creatively and efficiently with project managers, researchers, and faculty</li>
-							<li>Passion for civic engagement, civic technology, and/or humanitarian development</li>
-							<li>Passion for open source development and connecting to/leading communities of practice</li>
-							<li>Ability to work in a highly-team-based environment and contribute to design meetings</li>
-							<li>Ability to self-start and lead</li>
-							<li>Ability to make it past 10 pipes in Flappy Bird</li>
+							<li>Cultivating and maintaining a spirited and collaborative lab culture</li>
+							<li>Creating and managing grant, project, and operating budgets</li>
+							<li>Helping to develop and deliver Lab messaging (at conferences, to Lab visitors, in writing)</li>
+							<li>Maintaining healthy and productive client relationships</li>
+							<li>Assisting PIs in grant and contract writing</li>
+							<li>Managing all Lab staff, contractors, and students, and conducting appropriate meetings, workshops, and retreats</li>
+						</ul>
+						<h4>Creative Oversight</h4>
+						<ul>
+							<li>Working with PIs to guide overall creative approach, design methodologies, and community implementations across projects</li>
+							<li>Overseeing producers, programmers, and graphic designers in producing affective UX</li>
+							<li>Overseeing project managers and researchers in understanding appropriate sites for real-world interventions, identifying actionable problems, and cultivating community partnerships</li>
+							<li>Working and experimenting across modalities for effective community impact</li>
+						</ul>
+						<h4>Research Oversight</h4>
+						<ul>
+							<li>Building and maintaining Lab research capacity (both design research and basic research)</li>
+							<li>Working with faculty PIs to guide the Lab’s research agenda</li>
+							<li>Integrating design and research across projects</li>
+							<li>Cultivating an atmosphere of questioning, critical insight, and theoretical connections</li>
+						</ul>
+						<p>
+							The Managing Director reports directly to the Executive Director, and oversees a team of up to 15 staff, contractors, and students.
+						</p>
+						<h4>Required Experience</h4>
+						<p>Candidates should have at least:</p>
+						<ul>
+							<li>4 years of experience in a related field (which can include design research, civic engagement, media production, academic research, and nonprofit management)</li>
+							<li>A Bachelor Degree in a related discipline</li>
+							<li>Strong abilities to effectively manage staff</li>
+							<li>Ability to demonstrate an aptitude and desire to create and research civic media</li>
+							<li>Experience in design tools as well as competency with HTML and CSS and ability to work within a highly technical environment; good understanding of technical requirements</li>
+							<li>Experience in traditional and/or design research</li>
+						</ul>
+						<h4>Ideal Experience</h4>
+						<p>Ideal candidates will also have:</p>
+						<ul>
+							<li>Experience working in both US and international contexts</li>
+							<li>Competencies in media production, research, and UX design</li>
+							<li>A graduate degree in a related discipline</li>
+							<li>Experience in qualitative and quantitative research methods</li>
+							<li>Experience working in a university setting</li>
 						</ul>
 						<p>
 							If you are interested, please send a resume and cover letter to <span>steve [at] engagementgamelab.org</span>. 
