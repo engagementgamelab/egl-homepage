c1e5ff9e8da418c65678084c87381fdfb2ae72ab
jayvachon <jaylvachon@gmail.com>
2015-08-27 14:59:22 -0400

    update jobs

--- jobs/index.php
+++ jobs/index.php
@@ -10,42 +10,36 @@
 				<div class='row' id='jobs'>
 					<div class='span12'>
 						<h1>Come Work with Us!</h1>
-						<h3>We are not looking to fill any positions at this time.</h3>
+						<!-- <h3>We are not looking to fill any positions at this time.</h3> -->
+						<h3>We are looking to fill the following positions as soon as possible:</h3>
 
-
-
-                        <!--<h4><span>Position:</span> Contract Unity UI Engineer</h4>
+                        <h4><span>Position:</span> Research Assistant and Project Coordinator | Community-based Institutional Review Board (CIRB)</h4>
                         <p>
-                        	<span>Description:</span> The Engagement Lab at Emerson College is looking for a UI Engineer for a short-term contract (1-3 months) to help us develop a game built in the Unity 5 engine. This position requires extensive experience with Unity and C#, and the ability to produce high-quality, reusable code very quickly within a small team environment. The ideal candidate will have a keen UI/UX design sense and the programming ability to implement assets created by our team in a streamlined, mobile first development cycle.
+                        	<span>Description:</span> Boston-area community organizations are heavily studied. Academic researchers are consistently reaching out to these organizations for their data or their connections. Too often, this relationship is merely extractive - researchers take data, publish in academic journals, and never provide meaningful input to the organizations and the communities they serve. While researchers need to go through an institutional review process to assure the safety of research subjects, there is nothing in the current process that assures the benefit of research.
                         </p>
-						<h4>Ideals</h4>
-						<ul>
-							<li>at least 2 years of engineering experience, ideally within gaming</li>
-							<li>experience <i>with the newer Unity (4.6+) UI toolset</i>, or ability to learn it VERY quickly</li>
-							<li>strong background in C#</li>
-							<li>ability to rapidly prototype and iterate quickly</li>
-							<li>a fundamental understanding of responsive UI</li>
-							<li>able to think creatively and solve technical challenges</li>
-							<li>ability to work in a self-directed manner and in a collaborative team environment</li>
-							<li>consistently write well-structured and readable code</li>
-							<li>solid understanding of 2D/3D graphics</li>
-						</ul>
-						<h4>Bonuses</h4>
-						<ul>
-							<li>shipped 1 or more game titles</li>
-							<li>ability to work with tools such as Photoshop and/or Illustrator</li>
-							<li>experience using Git or similar SCM suites</li>
-							<li>experience managing interns or assistant developers</li>
-							<li>interested in working on educational/experimental games</li>
-						</ul>
+                        <p>
+                        	The Engagement Lab at Emerson College, in connection with the <a href="http://bostoncivic.media/">Boston Civic Media Consortium</a>, is spearheading an effort to create a Community Institutional Review Board (CIRB) - or a collaboratively written, and collectively ratified set of ethical standards that organizations can use to evaluate research partnerships and assure mutual benefit. The process of drafting the Boston-area CIRB will serve two primary purposes: 1) to build capacity of local organizations so that they understand what data they have and how to make it useful and impactful, and 2) to demystify the research process and research relationship in order to assure that organizations begin from a powerful negotiating position. 
+                        </p>
+						<h4>The EL is seeking a research assistant in the social sciences or engaged humanities to assist in the research and help coordinate the project under the guidance of Dr. Eric Gordon during the 2015-16 academic year.</h4>
+						<p>The CIRB will be developed using a participatory process that brings community stakeholders into a legitimate conversation with university researchers to understand and articulate mutually beneficial research practices. The research assistant’s responsibilities will include: </p>
+						<ol>
+							<li>In partnership with consortium members, help to coordinate and conduct 4-5 focus groups (with 6-8 community leaders each) to discuss how they can benefit from research, how they can share their data, and what protocols should be in place to assure maximum benefit to communities.</li>
+							<li>Coordinate and conduct 6-8 one on one interviews with organizational leaders, tech leaders and community groups and 4-6 interviews with faculty researchers.</li>
+							<li>Record, transcribe focus groups and interviews, then code using the qualitative analysis software Dedoose.</li>
+							<li>Collaboratively develop a CIRB document (no more than 5 pages that can be easily shared). We will work to have the document adopted by at least 10 community organizations in Boston. The CIRB will be a resource available to anyone to use.</li>
+							<li>Work with members of the Boston Civic Media Consortium and the City of Boston to integrate the CIRB in their work.</li>
+							<li>Assist in the preparation of academic publications in the fields of communication, civic technology, and methodological studies on participatory action research and co-design. </li>
+						</ol>
+						<h4>Why a Community IRB</h4>
 						<p>
-							Pay is negotiable and commensurate with experience. Local strongly preferred, but we will consider applicants who can work remotely.
+							When University faculty conduct research involving human subjects, they are required to work through an institutional review board (IRB) that reviews research protocols to assure compliance with federal ethics standards. IRB protocols were initially established to protect individual research subjects from physical and emotional harm and ultimately protect research institutions from liability. But they often fall short in ensuring that research is mutually beneficial for the researcher and the research subject. In the health fields, there have been some innovative efforts to create <a href="https://ccph.memberclicks.net/assets/Documents/FocusAreas/shaping_the_future_of_cenr.pdf">Community IRBs</a>, which are bottom-up ethics standards, written by and enforced by the populations most likely to be studied. As civic technology continues to be tested in cities throughout the world, and human subjects are routinely tapped to “test” tools and approaches, there is need to develop city-level Community IRBs specifically geared towards civic tech research.
+						</p>
+						<h4>About the new Boston Civic Media Consortium</h4>
+						<p>
+							The Community IRB is the first project within the new Boston Civic Media Consortium. The Engagement Lab is leading the effort to build a Civic Media Consortium in the Boston area. The Boston Civic Media Consortium is an informal network of faculty, labs and centers at Boston area universities focused on doing applied research at the intersection of media, technology and civic life. 
 						</p>
 						<p>
-							Please forward a resume/portfolio to <span>Jordan [at] elab.emerson.edu</span> along with a short bio/cover letter within the body of the email.
-						</p>-->
-						<p>
-							The Engagement Lab is an applied research lab at <a href="http://www.emerson.edu/">Emerson College</a> focusing on the development and study of games, technology, and new media to enhance civic life. The Lab works directly with its partner communities to design and facilitate civic engagement processes, augment stakeholder deliberation, and broaden the diversity of participants in local decision-making. Along with the <a href="salzburg.umd.edu">Salzburg Academy</a> on Media and Global Change, the Engagement Lab also serves as the hub of a global network of engagement and new media organizations.
+							Please forward a resume/portfolio to <span>sean [at] elab.emerson.edu</span> along with a short bio/cover letter within the body of the email.
 						</p>
 					</div>
 				</div>
